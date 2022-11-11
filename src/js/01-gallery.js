fa1bf499ac316c7fc8e05054039c198635b24f3b
Evgeniy Pavlichenko <yevheniipavl@gmail.com>
2022-11-11 18:16:03 +0200

    add task 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -3,3 +3,27 @@ import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+console.log(galleryItems);
+
+const galleryRef = document.querySelector('.gallery');
+// разметка
+function createGalleryPics(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<a class="gallery__item" href="${original}">
+      <img
+        class="gallery__image"
+        src="${preview}"
+        alt="${description}"
+      />
+    </a>`;
+    })
+    .join('');
+}
+galleryRef.insertAdjacentHTML('beforeend', createGalleryPics(galleryItems));
+
+var lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
