c3c7914d61c6e56d39e6f3b8f10e00496d203c81
Evgeniy Pavlichenko <yevheniipavl@gmail.com>
2022-11-13 19:01:12 +0200

    second done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,21 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const player = new Player('vimeo-player');
+// console.log(player);
+// записуем значение плэера в локалсторэдж
+function localStorageWrite(data) {
+  localStorage.setItem('videoplayer-current-time', data.seconds);
+  //   console.log(data.seconds);
+  //   console.log(data);
+}
+// добавляем сротл
+player.on('timeupdate', throttle(localStorageWrite, 1000));
+// сохраняет текущие время в (localStorage)
+player.setCurrentTime(localStorageRecord());
+// сохраняем значение видео
+function localStorageRecord() {
+  let savedValue = Number(localStorage.getItem('videoplayer-current-time'));
+  console.log(savedValue);
+  return savedValue;
+}
