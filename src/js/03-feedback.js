ee095146460c81537a8b6c28d70e530eae8355e3
Evgeniy Pavlichenko <yevheniipavl@gmail.com>
2022-11-15 16:56:07 +0200

    done

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -4,41 +4,73 @@ const formRef = document.querySelector('.feedback-form');
 // строка-ключ в локалсор.
 const LOCALSTORAGE_KEY = 'feedback-form-state';
 // объект для localStorage
-const userDate = {};
+let userDate = {};
+
 // функция для записи данных из локалстор. в форму
 const fillContactFormElements = () => {
   // получаем в переменную объект из локал сторидж, предварительно распарсив джейсон-строку
-  const userDataFromLS = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY));
-  //цыклом проходим по каждому ключу объекта
-  for (const prop in userDataFromLS) {
-    //   только по собственным свойствам
-    if (userDataFromLS.hasOwnProperty(prop)) {
-      // записуем данные с объекта в форму
-      formRef.elements[prop].value = userDataFromLS[prop];
-    }
+  const savedData = localStorage.getItem(LOCALSTORAGE_KEY);
+
+  if (savedData) {
+    const userDataFromLS = JSON.parse(savedData);
+    Object.entries(userDataFromLS).forEach(([name, value]) => {
+      // console.log(name);
+      // console.log(value);
+      formRef.elements[name].value = value;
+    });
   }
+  // //цыклом проходим по каждому ключу объекта
+  // for (const prop in userDataFromLS) {
+  //   //   только по собственным свойствам
+  //   if (userDataFromLS.hasOwnProperty(prop)) {
+  //     // записуем данные с объекта в форму
+  //     formRef.elements[prop].value = userDataFromLS[prop];
+  //   }
 };
-// вызов функции
-fillContactFormElements();
+if (localStorage.length !== 0) {
+  fillContactFormElements();
+}
+
 // Запись данных в локалсторэдж при введение в инпут
 const onContactFormInput = event => {
   // значения имени и данных записуем в переменные
   const name = event.target.name;
   const value = event.target.value;
+  // проверка, чтобы в локалстор. оставалось запол. поле
+  if (localStorage.getItem(LOCALSTORAGE_KEY)) {
+    userDate = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY));
+    // console.log(userDate);
+  }
   // имя и данные записуем в объект
   userDate[name] = value;
   //   console.log(userDate);
-  // записуем объект с новыми данными в локалстор., превратив его в стору
+  // записуем объект с новыми данными в локалстор., превратив его в строку
   localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(userDate));
 };
+
 // Очищение формы и локалстор. при сабмите
 const onContactFormSubmit = event => {
   event.preventDefault();
+  const submitedData = new FormData(formRef);
+  submitedData.forEach((value, name) => {
+    userDate[name] = value;
+  });
 
-  event.target.reset();
+  if (
+    formRef.elements.email.value === '' ||
+    formRef.elements.message.value === ''
+  ) {
+    alert('Заповніть всі поля');
+  } else {
+    console.log(userDate);
+  }
+
+  // console.dir(formRef.elements.email.value);
+
   localStorage.removeItem(LOCALSTORAGE_KEY);
-  console.log(userDate);
+  event.target.reset();
 };
+
 // слушатили событий
 formRef.addEventListener('input', throttle(onContactFormInput, 500));
 formRef.addEventListener('submit', onContactFormSubmit);
